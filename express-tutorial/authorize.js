cea27349844ca96a5c795af30a5321b8122cbe94
Stephen Kettley <kettleystephen@gmail.com>
2023-06-08 11:01:18 +0200

    update authorize function

--- express-tutorial/authorize.js
+++ express-tutorial/authorize.js
@@ -1,6 +1,11 @@
 const authorize = (req, res, next) => {
-  console.log("authorize");
-  next();
+  const { user } = req.query;
+  if (user === "steve") {
+    req.user = { name: "steve", id: 3 };
+    next();
+  } else {
+    res.status(401).send("unauthorized");
+  }
 };
 
 module.exports = authorize;
