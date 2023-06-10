d3dc6b1cfbd4f011a141ab5cb25b3fce419383c7
Stephen Kettley <kettleystephen@gmail.com>
2023-06-10 13:27:36 +0200

    update methods example

--- express-tutorial/final/10-methods.js
+++ express-tutorial/final/10-methods.js
@@ -25,6 +25,52 @@ app.post("/api/people", (req, res) => {
   res.status(201).json({ success: true, person: name });
 });
 
+app.post("/api/postman/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide a name" });
+  }
+  res.status(201).json({ success: true, data: [...people, name] });
+});
+
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((person) => person.id === Number(id));
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${id}` });
+  }
+
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+  res.status(200).json({ success: true, data: newPeople });
+});
+
+app.delete("/api/people/:id", (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id));
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${req.params.id}` });
+  }
+
+  const newPeople = people.filter(
+    (person) => person.id !== Number(req.params.id)
+  );
+  res.status(200).json({ success: true, data: newPeople });
+});
+
 app.post("/login", (req, res) => {
   const { name } = req.body;
   if (name) {
